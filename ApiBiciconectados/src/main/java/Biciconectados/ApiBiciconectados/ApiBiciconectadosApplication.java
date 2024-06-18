0f93a372f40e91c87424181a41a180dfb148f0dd
claudioandres22 <claudiorojas122@gmail.com>
2024-06-18 14:38:36 -0400

    api biciconectados

---
+++ ApiBiciconectados/src/main/java/Biciconectados/ApiBiciconectados/ApiBiciconectadosApplication.java
@@ -0,0 +1,13 @@
+package Biciconectados.ApiBiciconectados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBiciconectadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBiciconectadosApplication.class, args);
+	}
+
+}
---
+++ ApiBiciconectados/src/test/java/Biciconectados/ApiBiciconectados/ApiBiciconectadosApplicationTests.java
@@ -0,0 +1,13 @@
+package Biciconectados.ApiBiciconectados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBiciconectadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
